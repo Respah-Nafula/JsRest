08fd04dd483b23bf00fe547fd219484a3b3edb36
Your Name <nafularespah@gmail.com>
2022-08-19 13:58:16 +0300

    changes

--- fetching.js
+++ fetching.js
@@ -1,11 +1,39 @@
-function fetchData() {
-    fetch("http://127.0.0.1:5500/index.html")
+function fetchProducts() {
+    fetch("localhost:5500/index.html")
+      .then((response) => response.json())
+       .then(data => {
+        for(var product=0; product<data.length; product++){
+            document.getElementById("products").innerHTML += data[product].name + " " +data[product].category
+        }
+        console.log(data)
+        document.querySelector("fruList").innerText = data.category = "fruits"
+      
+        document.querySelector("vegList").innerText = data
+    })
+  }
+  fetchProducts();
+
+
+  function fetchfruList() {
+    fetch("localhost:5500/index.html")
       .then((response) => response.json())
       .then((json) => takeData(json));
+      for(var fruit=0; fruitdata.length; fruit++){
+        document.getElementById("fruList").innerHTML += data[fruit].name + " " +data[fruit].category
+
   }
-  
-  function takeData(val) {
-    return val.slice(1, 11);
+}
+  fetchfruList();
+
+
+  function vegList() {
+    fetch("localhost:5500/index.html")
+      .then((response) => response.json())
+      .then((json) => takeData(json));
+      for(var veg=0; veg<data.length; veg++){
+        document.getElementById("vegList").innerHTML += data[veg].name + " " +data[veg].category
   }
+}
+  vegList();
+
   
-  fetchData();
